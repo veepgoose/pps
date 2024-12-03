f8ba1c056be876ecd8bfac53c1b4155959067d32
veepgoose <victoriaplatt@loveplanes.co.uk>
2024-12-03 16:10:17 +0000

    Changed social icons - created outline and shadows

--- pixelpopstudio/src/components/SocialIcons.js
+++ pixelpopstudio/src/components/SocialIcons.js
@@ -1,23 +1,26 @@
 export default function SocialIcons() {
     return (
       <div className="flex justify-center items-center space-x-2">
-        <a href="https://www.linkedin.com/in/vplatt/" aria-label="LinkedIn">
+
+<a href="https://www.instagram.com/pixel.pop.studio" aria-label="Instagram">
           <img
-            src="/LinkedIconPink.svg"
-            alt="LinkedIn"
-            className="w-16 h-18"
-          />
-        </a>
-        <a href="https://www.instagram.com/pixel.pop.studio" aria-label="Instagram">
-          <img
-            src="/InstaIconPink.svg"
+            src="/InstaIconShadowT.svg"
             alt="Instagram"
             className="w-16 h-18"
           />
         </a>
+        
+        <a href="https://www.linkedin.com/in/vplatt/" aria-label="LinkedIn">
+          <img
+            src="/LinkedIconShadowT.svg"
+            alt="LinkedIn"
+            className="w-16 h-18"
+          />
+        </a>
+     
         <a href="mailto:vic@pixelpopstudio.co.uk" aria-label="Email">
           <img
-            src="/EmailIconPink.svg"
+            src="/EmailIconShadowT.svg"
             alt="Email"
             className="w-16 h-18"
           />
